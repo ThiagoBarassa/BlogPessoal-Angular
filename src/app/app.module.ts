97c0591d1677587c8944baf07b343bb2636c494a
ThiagoBarassa <thiagobarassa@gmail.com>
2021-01-18 22:39:53 -0300

    Primeiros passos com o layout da pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
