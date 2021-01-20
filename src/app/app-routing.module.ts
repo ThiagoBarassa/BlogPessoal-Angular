79952ce38b103371d2a12a636509231f3580c061
ThiagoBarassa <thiagobarassa@gmail.com>
2021-01-20 08:23:46 -0300

    adicionando novas funcionalidades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path:'entrar', component:LoginComponent},
+  {path:'cadastro', component:CadastroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
